56c6883d816359edba8b374630e6d73e90296807
Owen Kwon <pinebud77@hotmail.com>
2018-06-18 01:06:52 -0700

    store day report error fix

--- stock/main.py
+++ stock/main.py
@@ -58,6 +58,12 @@ def store_db_stock(db_stock, stock):
 
 
 def store_day_report(db_account, dt):
+    try:
+        prev_report = models.DayReport().objects.filter(account=db_account, dt__lt=dt).order_by('-date')[0]
+        if prev_report.date.year == dt.year and prev_report.date.month == dt.month and prev_report.date.day == dt.day:
+            prev_report.delete()
+    except IndexError:
+        pass
     day_report = models.DayReport()
     day_report.date = dt
     day_report.account = db_account
@@ -126,7 +132,7 @@ def run(dt=None):
         result = client.login(dt)
     if not result:
         logging.error('login failed')
-        exit(-1)
+        return
     logging.debug('logged in')
 
     alg_ahnyung = AhnyungAlgorithm()
