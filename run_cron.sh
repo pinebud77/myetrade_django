20ff872760f6c60f36865e6cb21aaf27a59f1a7c
Owen Kwon <pinebud77@hotmail.com>
2019-02-22 02:45:44 -0500

    sort out auto run script

--- run_cron.sh
+++ run_cron.sh
@@ -11,4 +11,5 @@ fi
 
 echo "non-holiday"
 cd ${DJANGO_BASE}/logs
-wget --timeout=1000 --no-check-certificate -O ${1}.log https://127.0.0.1/stock/${1}
+wget --timeout=1000 --no-check-certificate -O get_history.log https://127.0.0.1/stock/get_history
+wget --timeout=1000 --no-check-certificate -O run.log https://127.0.0.1/stock/run
